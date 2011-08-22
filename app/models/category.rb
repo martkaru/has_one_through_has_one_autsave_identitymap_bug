6cee7a0657d9702a42461a490351c6c2d69a85c3
Mart Karu <karu@metal.ee>
2011-08-22 12:16:14 +0300

    Fancier variable names :)

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,16 +1,16 @@
 class Category < ActiveRecord::Base
   has_one :parent_connection, :as => :to, :class_name => 'CategoryConnection', :dependent => :destroy
-  has_one :p, :through => :parent_connection, :source_type => 'Category', :source => :from
+  has_one :parent, :through => :parent_connection, :source_type => 'Category', :source => :from
 
   def self.test
     CategoryConnection.destroy_all
 
-    p = Category.find 1
-    c = Category.find 2
+    parent = Category.find 1
+    child = Category.find 2
 
-    c.p = p
-    c.save
+    child.parent = parent
+    child.save
 
-    return p.p ? 'fail' : 'OK!'
+    return parent.parent ? 'fail' : 'OK!'
   end
 end
